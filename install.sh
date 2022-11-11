4df0ab50e500a3390f9772986c960c4178e51de6
Daniel Roseman <daniel.roseman@shopify.com>
2022-11-11 14:59:10 +0000

    Spin changes
    Move package installs to packages.yml
    
    Config nvim instead of vim

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #! /bin/sh
-FILES="vimrc zshrc gitconfig gitattributes gitignore_global pryrc"
+FILES="zshrc gitconfig gitattributes gitignore_global pryrc ripgreprc"
 SOURCE=$PWD
 TARGET=$HOME
 
 for f in $FILES; do
 	ln -sf $SOURCE/$f $TARGET/.$f;
 done
+ln -sf $PWD/config/nvim ~/.config/nvim
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nix-env -i bat
-nix-env -i fd
-nix-env -iA nixpkgs.delta
+nvim -es -i NONE -c "PlugInstall" -c "qa"
